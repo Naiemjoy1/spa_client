236cc1b5e250d7653caf8af3d2ca3fd68571aa44
naiemjoy1 <naiemunhasan@gmail.com>
2025-02-15 23:59:48 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        openSans: "'Open Sans', sans-serif ",
+        Playfair: "'Playfair Display', serif",
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#12B788",
+          secondary: "#f44369",
+          accent: "#FFD700",
+          neutral: "#0d1224",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "synthwave",
+    ],
+  },
+}
