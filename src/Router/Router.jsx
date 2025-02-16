f15cf2fef3d8e2a151220893ece2b63847484e71
naiemjoy1 <naiemunhasan@gmail.com>
2025-02-17 01:18:02 +0600

    nav link and logo and fav

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,20 +1,38 @@
-import {
-    createBrowserRouter
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Blog from "../Pages/Blog/Blog";
+import Booking from "../Pages/Booking/Booking";
 import Home from "../Pages/Home/Home";
+import Service from "../Pages/Service/Service";
+import Shop from "../Pages/Shop/Shop";
 import Error from "../Shared/Error/Error";
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      errorElement:<Error></Error>,
-      children:[
-        {
-            path:'/',
-            element:<Home></Home>
-        }
-      ]
-    },
-  ]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/service",
+        element: <Service></Service>,
+      },
+      {
+        path: "/booking",
+        element: <Booking></Booking>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/shop",
+        element: <Shop></Shop>,
+      },
+    ],
+  },
+]);
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,18 +1,52 @@
+import { FaSearch } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import logo from "../../assets/Images/logo/logo.png";
+
 const Navbar = () => {
+  const currentPath = window.location.pathname;
+
+  const navLink = (
+    <>
+      {["Home", "Service", "Booking", "Blog", "Shop"].map((item) => {
+        const path = item === "Home" ? "/" : `/${item.toLowerCase()}`;
+        return (
+          <li
+            className="relative w-20 flex justify-center items-center"
+            key={item}
+          >
+            <a
+              href={path}
+              className={`inline-block text-center w-full py-2 hover:text-blue-500 
+                after:absolute after:left-0 after:bottom-0 after:w-full 
+                after:h-0.5 after:bg-blue-500 after:transition-transform 
+                after:duration-300 ${
+                  currentPath === path
+                    ? "after:scale-x-100 text-blue-500"
+                    : "after:scale-x-0 hover:after:scale-x-100"
+                }`}
+            >
+              {item}
+            </a>
+          </li>
+        );
+      })}
+    </>
+  );
+
   return (
     <div className="container mx-auto">
-      <div className="flex">
+      <div className="flex items-center justify-between">
         <div className="w-1/4">
-          <p>left</p>
+          <img src={logo} alt="Logo" className="h-20 w-auto" />
         </div>
-        <div className="w-9/12">
-          <ul className="menu menu-horizontal space-x-2">
-            <li>Home</li>
-            <li>Service</li>
-            <li>Booking</li>
-            <li>Blog</li>
-            <li>Shop</li>
+        <div className="w-9/12 flex justify-end">
+          <ul className="menu menu-horizontal space-x-6 font-medium">
+            {navLink}
           </ul>
+          <div className="flex items-center gap-6 text-lg">
+            <FaSearch />
+            <IoCartOutline />
+          </div>
         </div>
       </div>
     </div>
